f37a0c8fdad3903b958d7b8f79781559bb7751ba
Sebastian Bartuzi <sebastian.bartuzi@student.manchester.ac.uk>
2022-03-09 19:44:25 +0000

    Basic conversation exchange mechanism

--- imclient_h86745sb.py
+++ imclient_h86745sb.py
@@ -5,7 +5,7 @@ server = im.IMServerProxy('https://web.cs.manchester.ac.uk/h86745sb/COMP28112_ex
 
 if server['user1'] == b'logged\n' and server['user2'] == b'logged\n':
     print("Two users are logged in. Wait until one of them logs off.")
-    while (server['user1'] == b'logged\n' and server['user2'] == b'logged\n'):
+    while (server['user1'] == b'logged\n' and server['user2'] == b'logged\n'): #Wait until someone logs off
         time.sleep(0.5)
 
 if server['user1'] == b'unlogged\n': # user1 is not logged in. Log in as user1
@@ -22,11 +22,37 @@ else: # user1 is logged in. user2 is not logged in. Log in as user2
     user = True # True is for user2
     print("Logged in as user2.")
 
-# while True:
-#     if responding:
-#         myMessage = input("Type your message: ")
-#         server['message'] = myMessage
-#         responding = false
-#         waiting = true
-#     if waiting:
-#         while ()
+# Both users are logged in. Start conversation
+
+# Give users initial states
+if user == False: # user1
+    responding = True
+    waiting = False
+else: # user2
+    responding = False
+    waiting = True
+
+conversationHistory = []
+server['userTurn'] = '1'
+while True:
+    if responding:
+        message = input("Type your message: ")
+        conversationHistory.append(message)
+        server['lastMessage'] = message
+        if user == False: # user1
+            server['userTurn'] = '2' # It is turn for user2 to read and reply
+        else: # user2
+            server['userTurn'] = '1' # It is turn for user1 to read and reply
+        responding = False
+        waiting = True
+    if waiting:
+        if user == False: # user1
+            while server['userTurn'] == b'2\n': # user1 waits for their turn
+                time.sleep(0.5)
+            print("User2: " + server['lastMessage'].decode('UTF-8'))
+        else: # user2
+            while server['userTurn'] == b'1\n': # user2 waits for their turn
+                time.sleep(0.5)
+            print("User1: " + server['lastMessage'].decode('UTF-8'))
+        responding = True
+        waiting = False
